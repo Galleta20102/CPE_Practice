b416143bced0c1b5a1a7e4d5259739d9aabb7c94
Galleta20102 <80142967+Galleta20102@users.noreply.github.com>
2021-03-17 13:19:10 +0800

    UVA10055 Update

---
+++ UVA10055.cpp
@@ -0,0 +1,14 @@
+//20210317
+
+//https://onlinejudge.org/external/100/10055.pdf
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int a,b;
+	while(cin>>a>>b){
+		cout<<abs(a-b)<<endl;
+	}
+}
