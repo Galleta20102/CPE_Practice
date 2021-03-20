a4b97dc356d3d19352e73b0f62be539a9c308ab8
Galleta20102 <80142967+Galleta20102@users.noreply.github.com>
2021-03-20 22:26:59 +0800

    Update UVA10071

---
+++ UVA10071.cpp
@@ -0,0 +1,14 @@
+//20210320
+
+//https://onlinejudge.org/external/100/10071.pdf
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int a,b;
+	while(cin>>a>>b){
+		cout<<2*a*b<<endl;
+	}
+}
